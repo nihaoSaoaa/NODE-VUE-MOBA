b3fca98abdccafb6b21332354df65f6ace6e6b9a
Ma <1036752927@qq.com>
2020-06-05 11:14:16 +0800

    '购物车本地化;history模式后台支持'

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const cors = require('cors')
+const history = require('connect-history-api-fallback')
 const app = express()
 const port = 3000
 // 模拟全局秘钥
 app.set('SECRET', 'i2u34y12oi3u4y8')
-
+// 跨域
 app.use(cors())
+// 数据 json 化
 app.use(express.json())
+// history模式兼容
+app.use(history({
+  rewrites: [
+    { from: /^\/(home|cart|category|profile:id|detail:id)\/.*$/, to: './mall/index.html' },
+    { from: /^\/admin\/.*$/, to: './admin/index.html' }
+  ]
+}))
 
 // 静态文件可访问
 app.use('/uploads', express.static(__dirname + '/uploads'))
